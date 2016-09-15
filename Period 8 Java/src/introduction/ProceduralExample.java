18f3cb64ba3ec82bc2080a0fdd91ac53445effd8
Student 8 <Student 8@10.8.33.105>
2016-09-15 13:41:31 -0400

    Jillian says her name!!!

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,8 +2,8 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello World");
-    System.out.println("sure");
+	 String[] names = {"Jilian", "Joseph", "Jordan"};
+    System.out.println("Hi, my name is" + names[0] + ".");
   }
 }
 
