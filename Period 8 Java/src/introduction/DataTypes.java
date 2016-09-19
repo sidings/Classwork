720f39e9aaf386f793a47f802cea9e86e2371935
Student 8 <Student 8@10.8.33.105>
2016-09-19 14:00:38 -0400

    cool

---
+++ Period 8 Java/src/introduction/DataTypes.java
@@ -0,0 +1,49 @@
+package introduction;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		//System.out.println((int)(3.0 + 5)/(2 + 5 * 2));
+		//if(42 == 42.0) System.out.println("42 equals 42.0");
+	/*	double d1 = 4.64; 
+		double d2 = 2.0;
+		double d3 = 2.64;
+
+		System.out.println("d1 : " + d1); 
+		System.out.println("d2 : " + d2); 
+		System.out.println("d3 : " + d3); 
+		System.out.println("d1 - d2 : " + (d1 - d2));*/
+		
+		String s = 1;
+		System.out.println(s);
+
+		String s = (String)1;
+		System.out.println(s);
+
+		String s = "1";
+		System.out.println(s);
+
+		String s = ""+1;
+		System.out.println(s);
+
+		String s = "Happy "+18+"th birthday!";
+		System.out.println(s);
+
+		String s = "Happy 18th birthday!";
+		System.out.println(s);
+
+		String s = "Happy "+54/3+"th birthday!";
+		System.out.println(s);
+
+		String s = "Happy "+(54/3)+"th birthday!";
+		System.out.println(s);
+		int m = 22;
+		int n = 7;
+		System.out.println("m/n is "+(m/n));
+		
+		int m = 22;
+		int n = 7;
+		System.out.println("m/n is "+((double)m/n));
+	}
+
+}
