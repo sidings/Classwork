8bc1c1e22f2b64215a011647aa999652899cc842
Student 8 <Student 8@10.8.33.105>
2016-09-20 14:03:27 -0400

    ok

--- Period 8 Java/src/introduction/DataTypes.java
+++ Period 8 Java/src/introduction/DataTypes.java
@@ -5,16 +5,16 @@ public class DataTypes {
 	public static void main(String[] args) {
 		//System.out.println((int)(3.0 + 5)/(2 + 5 * 2));
 		//if(42 == 42.0) System.out.println("42 equals 42.0");
-	/*	double d1 = 4.64; 
+		double d1 = 4.64; 
 		double d2 = 2.0;
 		double d3 = 2.64;
 
-		System.out.println("d1 : " + d1); 
+		/*System.out.println("d1 : " + d1); 
 		System.out.println("d2 : " + d2); 
 		System.out.println("d3 : " + d3); 
 		System.out.println("d1 - d2 : " + (d1 - d2));*/
 		
-		String s = 1;
+		/*String s = 1;
 		System.out.println(s);
 
 		String s = (String)1;
@@ -40,10 +40,14 @@ public class DataTypes {
 		int m = 22;
 		int n = 7;
 		System.out.println("m/n is "+(m/n));
-		
+		 
 		int m = 22;
 		int n = 7;
-		System.out.println("m/n is "+((double)m/n));
+		System.out.println("m/n is "+((double)m/n));*/
+	
+		/*String ten = "10";
+		String two = "2";
+		if (ten.compareTo(two) > 0) System.out.println("10 is greater than 2");
+		else System.out.println("10 is not greater than 2");*/
+		}
 	}
-
-}
---
+++ Period 8 Java/src/introduction/RandomDiceRoll.java
@@ -0,0 +1,33 @@
+package introduction;
+
+public class RandomDiceRoll {
+
+	public static void main(String[] args) {
+		int[] results = new int[6];//fill array with 6 zeroes
+		
+		//declare variable, logic test, increment
+		for(int index = 0; index < 1000; index ++){
+			int result = rollFairDie();
+			//if(index%2 == 0) System.out.println("Roll #" + (index + 1)+ ":" + rollFairDie());
+			//else System.out.println("Roll #" + (index + 1) + ":" + rollUnfairDie());
+			//System.out.println("Roll #" + (index + 1) + ":" + result);
+			results[result-1] ++;
+		}
+		//print the results
+		for (int i = 0; i<6; i++){
+			System.out.println((i + 1) + " appeared " + results[i] + " times.");
+		}
+			
+	}
+	//return 1,2,3,4,5,6 with equal probability
+	public static int rollFairDie(){
+		double rand = Math.random();//returns a double between 0 and 1
+		//int roll = (int) (6*rand);//[0, 5]
+		int roll = (int) (6*rand)+1;//[1, 6]
+		return roll;
+	}
+	public static int rollUnfairDie(){
+		return 1;
+	}
+}
+
