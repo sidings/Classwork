7d19e9fc9bd265ac6e356817309e21afb387a5b8
sidings <jrna.fng@gmail.com>
2016-10-17 14:01:28 -0400

    heyoo

--- Period 8 Java/src/arrays/ArraysPractice.java
+++ Period 8 Java/src/arrays/ArraysPractice.java
@@ -5,12 +5,49 @@ public class ArraysPractice {
 	static boolean[] boos3;
 	
 	public static void main(String[] args) {
+		//how do you time a process?
+		long currentTime =  System.currentTimeMillis();
+		
+		String[] someStrings = new String[1000];
+		standardPopulate(someStrings);
+		String s = someStrings[999];
+		makeSpecial(s);
+		print(someStrings);
+		
+		initializingArraysExample();
+		long endTime = System.currentTimeMillis();
+		System.out.println("The process took " + (endTime-currentTime) + " milliseconds.");
+		
+	}
+	
+	private static void makeSpecial(String str) {
+		str = "THIS STRING IS SPECIAL";
+	}
+
+	private static void print(String[] arr) {
+		for (int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
+	}
+
+	private static void standardPopulate(String[] arr) {
+		/**for (int i = 0; i< arr.length; i++){
+			arr[i] = "String #" + (i+1);
+		}*/
+		int i = 0; 
+		for (String x: arr){
+			i++;
+			x = "String #" + i;
+		}
+	}
+
+	public static void initializingArraysExample(){
 		//2 different ways 
 		boolean[] boos1 = new boolean[3];
 		// this can ONLY be done at the declaration because it sets size and content:
 		boolean[] boos2 = {false, false, false};
 		//this does NOT work:
-			//boos3 = {false, true, true}; 
+		//boos3 = {false, true, true}; 
 		//this is all that will work:
 		boos3 = new boolean[3];
 		
@@ -19,7 +56,7 @@ public class ArraysPractice {
 			STANDARD 'FOR' LOOP:
 				- the index is important to keep track of
 				- you need to customize the order
-		*/
+		 */
 		
 		/*
 		 	if "boolean[] boos1 = new boolean[3]" then returns false, false, false
@@ -52,6 +89,6 @@ public class ArraysPractice {
 		for(String s: someStrings1){
 			System.out.println(s);
 		}
+		
 	}
-
 }
