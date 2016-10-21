6bc9ce5ccce9d376786b761cc0d74e66581abca6
sidings <jrna.fng@gmail.com>
2016-10-21 14:03:53 -0400

    heyo

--- Period 8 Java/src/arrays/AdvancedArraysMethods.java
+++ Period 8 Java/src/arrays/AdvancedArraysMethods.java
@@ -5,10 +5,17 @@ public class AdvancedArraysMethods {
 	public static void main(String[] args) {
 		
 		String[] array = {"a", "b", "c", "d", "e", "f", "g", "h"};
-		swap(array, 0, array.length-1);
+		//swap(array, 0, array.length-1);
+		shuffle(array);
 	}
-	private static void swap(String[] arr, int a, int b) {
-		String placeholder = arr[b];
+	private static void shuffle(Object[] array) {
+		for (int i = 0; i<array.length; i++){
+			int random = (int) (Math.random()*6);
+			swap(array, i, random);
+		}
+	}
+	private static void swap(Object[] arr, int a, int b) {
+		Object placeholder = arr[b];
 		arr[b] = arr[a];
 		arr[a] = placeholder;
 	}
