76c9339c1f342ac745d3765b919bda0a3ebaf703
sidings <jrna.fng@gmail.com>
2016-12-01 14:04:05 -0500

    n

--- Period 8 Java/src/sortingAlgorithms/RecursionIntro.java
+++ Period 8 Java/src/sortingAlgorithms/RecursionIntro.java
@@ -3,27 +3,39 @@ package sortingAlgorithms;
 public class RecursionIntro {
 
 	public static void main(String[] args) {
-		//System.out.println("Using a for loop:");
+		/*System.out.println("Using a for loop:");
 		for (int i = 0; i < 5; i++){
-		//	System.out.println("Hello, World! x" + i);
+			System.out.println("Hello, World! x" + i);
 		}
-		//System.out.println("Without using a for loop:");
+		System.out.println("Without using a for loop:");
 		forLoop(5, new Action(){
 			private int value = 0;
 			public void act(){
-			//	System.out.println("Hello, World! x" + value);
+				System.out.println("Hello, World! x" + value);
 				value++;
 			}
 		});
-		//factorial
+		factorial(9);
 		int integer = 5;
 		for(int i = integer-1; i > 0; i--){
 			integer = integer*i;
 		}
-		//System.out.print(integer);
-		//System.out.print(factorial(5));
+		System.out.print(integer);
+		System.out.print(factorial(5));
 		
-		hanoiSolution(3, "a", "b", "c");
+		hanoiSolution(3, "a", "b", "c");*/
+		int n = 10; 
+		System.out.println("The " + n + "th Fibonacci is " + fibonacci(n));
+	}
+
+	private static int fibonacci(int n) {
+		if(n <= 1) return 1;
+		else{
+			int previous = fibonacci(n-1);
+			System.out.println("Before we find Fibonacci " + n + ", we need to find Fibonacci " + (n-1) + ", which is " + previous);
+			int beforePrev = fibonacci(n-2);
+			return previous  + beforePrev;
+		}
 	}
 
 	private static void forLoop(int i, Action action) {
@@ -43,11 +55,19 @@ public class RecursionIntro {
 			return i*factorial(i-1);
 		}
 	}
+	private static int count = 1;
+	
+	public static void print(String s){
+		System.out.println("Move #" +count + ":" + s);
+		count++;
+	}
 	
 	public static void hanoiSolution(int numOfDiscs, String startPeg, String midPeg, String endPeg){
 		if (numOfDiscs <= 1){
-			System.out.println("Move " + startPeg + " to " + endPeg);
+			print("Move " + startPeg + " to " + endPeg);
 		}else{
+			System.out.println("In order to move " + numOfDiscs + " over to peg " + endPeg + ", we must move " + (numOfDiscs 
+					-1) + " over to peg " + midPeg + " first.");
 			hanoiSolution(numOfDiscs-1, startPeg, endPeg, midPeg);
 			hanoiSolution(1, startPeg, midPeg, endPeg);
 			hanoiSolution(numOfDiscs-1, midPeg, startPeg, endPeg);
