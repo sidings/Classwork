37f502ada72934ee5798cd5527efeefe87b5d439
sidings <jrna.fng@gmail.com>
2016-12-07 14:03:12 -0500

    heuo

---
+++ Period 8 Java/src/guiPractice/GUIApplication.java
@@ -0,0 +1,36 @@
+package guiPractice;
+
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class GUIApplication extends JFrame{
+
+	private Screen currentScreen;
+	
+	//main method for practice only
+	public static void main(String[] args) {
+		new GUIApplication();
+	}
+	
+	public GUIApplication(){
+		//terminate program when window is closed
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		int x = 40;
+		int y = 40;
+		int width = 600;
+		int height = 400;
+		setBounds(x, y, width, height);
+		initScreen();
+		setVisible(true);
+	}
+
+	protected void initScreen() {
+		Screen startScreen = new Screen(getWidth(), getHeight());
+		currentScreen = startScreen;
+	}
+	
+	public void paint(Graphics g){
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
+	}
+}
