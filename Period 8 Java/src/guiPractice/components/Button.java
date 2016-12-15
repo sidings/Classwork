24e794af4eb0a6ed670502898a05c3ff8be376f8
sidings <jrna.fng@gmail.com>
2016-12-15 13:50:36 -0500

    COOL BUTTON

--- Period 8 Java/src/guiPractice/components/Button.java
+++ Period 8 Java/src/guiPractice/components/Button.java
@@ -2,6 +2,7 @@ package guiPractice.components;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
@@ -17,16 +18,36 @@ public class Button extends TextLabel implements Clickable{
 		update();
 	}
 	
+	public Color getColor(){
+		return color;
+	}
+	
+	public void setColor(Color c){
+		color = c;
+		update();
+	}
+	
 	public void update(Graphics2D g) {
 		g = clear(); //clears image and gets new graphics
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		g.setColor(Color.black);
-		if(getText() != null){
-			g.setFont(new Font(getFont(), Font.PLAIN, getSize()));
-			g.drawString(getText(), 4, getHeight()-5);
-		}
-		g.drawRoundRect(0, 0, getWidth(), getHeight(), 30, 30);
+		g.setColor(color);
 		g.fillRoundRect(0, 0, getWidth(), getHeight(), 30, 30);
+		g.setColor(Color.black);
+		g.drawRoundRect(0, 0, getWidth(), getHeight(), 30, 30);
+		g.setFont(new Font(getFont(), Font.PLAIN, getSize()));
+		FontMetrics fm = g.getFontMetrics();
+		
+		if(getText() != null){
+			g.setColor(Color.white);
+			String t = getText();
+			//just in case the the text is too wide, cut off
+			int cutoff = t.length();
+			while(cutoff > 0 && fm.stringWidth(t) > getWidth()){
+				cutoff --;
+				t = t.substring(0, cutoff);
+			}
+			g.drawString(t, (getWidth()-fm.stringWidth(t))/2, (getHeight() + fm.getHeight() - fm.getDescent())/2);
+		}
 	}
 
 	@Override
---
+++ Period 8 Java/src/guiPractice/sampleGames/Test2Review.java
@@ -0,0 +1,63 @@
+package guiPractice.sampleGames;
+
+public class Test2Review {
+
+	private static boolean[][] grid;
+	public static void main(String[] args) {
+		grid = new boolean[5][5];
+		printGrid(grid);
+		buttonClick(3, 3);
+		printGrid(grid);
+		buttonClick(3, 3);
+		printGrid(grid);
+		
+	}
+	
+	public static void switchRow(boolean[] row, int x){
+		//switch row[x-1], row[x], row[x+1] IF THEY EXIST
+		for(int i = -1; i < 2; i++){
+			if (x + i >= 0 && x + i < row.length)row[x+i] = !row[x+i];	
+		}
+	}
+	
+	public static void switchColumn(boolean[][] grid, int r, int c){
+		// switch ABOVE and BELOW the value in row r and column c
+		switchIfValid(grid, r-1, c);
+		switchIfValid(grid, r+1, c);
+	}
+	
+	public static void switchIfValid(boolean[][] grid, int r, int c){
+		if(r >= 0 && r < grid.length && c >= 0 && c < grid[r].length){
+			grid[r][c] = !grid[r][c];
+		}
+	}
+	
+	public static void buttonClick(int r, int c) {
+		if (r >= 0 && r < grid.length){
+			switchRow(grid[r], c);
+		}
+		switchColumn(grid, r ,c);
+		//all above is 1 point, any error = 0
+		
+		//check if a single light is on
+		for (int row = 0; row < grid.length; row++){
+			for (int col = 0; col < grid[0].length; col++){
+				//1 of 2 points
+				if(grid[row][col])return;//1 of 2 points
+			}
+		}
+		System.out.println("You have solved the puzzle.");
+	}
+
+	public static void printGrid(boolean[][] gr){
+		for (int i = 0; i < gr.length; i ++){
+			for (int j = 0; j < gr[0].length; j++){
+				if (gr[i][j] == true) System.out.print("O");
+				else System.out.print("X");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("- - - - - - - - -");
+	}
+
+}
