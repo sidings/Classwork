26c7ca5ff6c054fe9a1397765c44bbfdc244466f
sidings <jrna.fng@gmail.com>
2016-12-16 14:04:03 -0500

    add picture

--- Period 8 Java/src/guiPractice/components/Graphic.java
+++ Period 8 Java/src/guiPractice/components/Graphic.java
@@ -1,6 +1,9 @@
 package guiPractice.components;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
 
 public class Graphic implements Visible {
 
@@ -13,48 +16,69 @@ public class Graphic implements Visible {
 		this.x = x;
 		this.y = y;
 		loadedImages = false;
-		loadImages(imageLocation, 0.0);
+		loadImages(imageLocation, 0, 0);
 	}
 	
-	public BufferedImage getImage() {
+	private void loadImages(String imageLocation, int w, int h) {
+		try{
+			//get image from file (FULL SIZE)
+			ImageIcon icon = new ImageIcon(imageLocation);
+			if(x == 0 && h == 0){
+				//use original size
+				image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
+				
+				//draw icon onto image
+				Graphics2D g = image.createGraphics();
+				g.drawImage(icon.getImage(), 0, 0, null);
+			}else{
+				//use custom size
+	
+			}
+			loadedImages = true;
+		}catch(Exception e){
+			//in case file is not found
+			e.printStackTrace();
+		}
+	}
+
+	private void loadImages(String imageLocation, double scale) {
 		// TODO Auto-generated method stub
-		return null;
+		
+	}
+
+	
+	public BufferedImage getImage() {
+		return image;
 	}
 
 	@Override
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 	@Override
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 	@Override
 	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
+		return image.getWidth();
 	}
 
 	@Override
 	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
+		return image.getHeight();
 	}
 
 	@Override
 	public boolean isAnimated() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-
+		//does nothing, image never changes
 	}
 
 }
