efbf31bea472095740f369eda5f2466f8bfe40d3
sidings <jrna.fng@gmail.com>
2016-12-21 14:01:52 -0500

    clickableScreen

---
+++ Period 8 Java/src/guiPractice/sampleGames/ClickableScreen.java
@@ -0,0 +1,70 @@
+package guiPractice.sampleGames;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import guiPractice.Screen;
+import guiPractice.components.Clickable;
+import guiPractice.components.Visible;
+
+public abstract class ClickableScreen extends Screen implements MouseListener {
+
+	private ArrayList<Clickable> clickables;
+	
+	public abstract void initAllObjects(ArrayList<Visible> visibles);
+	
+	public ClickableScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		for (int i = 0; i < clickables.size(); i++){
+			if(clickables.get(i).isHovered(e.getX(), e.getY())){
+				clickables.get(i).act();
+					break;
+			}
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public MouseListener getMouseListener(){
+		return this;
+	}
+	
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		initAllObjects(viewObjects);
+		clickables = new ArrayList<Clickable>();
+		for (int i = 0; i < viewObjects.size(); i ++){
+			if (viewObjects.get(i) instanceof Clickable)clickables.add((Clickable) viewObjects.get(i));
+		}
+	}
+	
+
+}
---
+++ Period 8 Java/src/guiPractice/sampleGames/MyPracticeClickableScreen.java
@@ -0,0 +1,58 @@
+package guiPractice.sampleGames;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import guiPractice.Screen;
+import guiPractice.components.Visible;
+
+public class MyPracticeClickableScreen extends ClickableScreen implements MouseListener {
+
+	public MyPracticeClickableScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initObjects(ArrayList<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> visibles) {
+		
+	}
+
+}
