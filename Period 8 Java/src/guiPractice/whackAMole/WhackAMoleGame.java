0514bfa674f3fff6b3477063e3397893eb0dde88
sidings <jrna.fng@gmail.com>
2016-12-22 14:01:39 -0500

    whack a mole

---
+++ Period 8 Java/src/guiPractice/whackAMole/WhackAMoleGame.java
@@ -0,0 +1,23 @@
+package guiPractice.whackAMole;
+
+import guiPractice.GUIApplication;
+
+public class WhackAMoleGame extends GUIApplication{
+
+	public WhackAMoleGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		WhackAMoleGame game = new WhackAMoleGame();
+		Thread app = new Thread(game);
+		app.start();
+	}
+
+	@Override
+	protected void initScreen() {
+		WhackAMoleScreen wams = new WhackAMoleScreen(getWidth(), getHeight());
+		setScreen(wams);
+	}
+
+}
