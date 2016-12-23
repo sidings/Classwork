723af306f8ed6f44e1d6bc8ecdd3e92a5f3e0486
sidings <jrna.fng@gmail.com>
2016-12-23 13:09:34 -0500

    cool

--- Period 8 Java/src/guiPractice/Screen.java
+++ Period 8 Java/src/guiPractice/Screen.java
@@ -122,5 +122,12 @@ public abstract class Screen {
 		viewObjects.add(v);
 	}
 
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
 
 }
--- Period 8 Java/src/guiPractice/whackAMole/WhackAMoleScreen.java
+++ Period 8 Java/src/guiPractice/whackAMole/WhackAMoleScreen.java
@@ -2,20 +2,71 @@ package guiPractice.whackAMole;
 
 import java.util.ArrayList;
 
+import guiPractice.components.TextLabel;
 import guiPractice.components.Visible;
 import guiPractice.sampleGames.ClickableScreen;
 
-public class WhackAMoleScreen extends ClickableScreen {
+public class WhackAMoleScreen extends ClickableScreen implements Runnable{
 
+	 private ArrayList<MoleInterface> moles;
+	 private PlayerInterface player;
+	 private TextLabel label;
+	 private TextLabel timeLabel;
+	 private double timeLeft;
+	
 	public WhackAMoleScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		timeLeft = 60.0;
+		Thread play = new Thread(this);
+		play.start();
 	}
 
 	@Override
 	public void initAllObjects(ArrayList<Visible> visibles) {
-		// TODO Auto-generated method stub
-
+		moles = new ArrayList<MoleInterface>();
+		//player = getAPlayer();
+		label = new TextLabel(getWidth()/2 - 60, getHeight()/2 - 30, 120, 60, "Ready...");
+		timeLabel = new TextLabel(getWidth()/2 - 60, 50, 120, 60, "");
+		visibles.add(label);
+		//visibles.add(player);
+		visibles.add(timeLabel);
 	}
+	
+	public void update(){
+		super.update();
+	}
+	
+	public void changeText(String s){
+		try{
+			Thread.sleep(1000);
+			label.setText(s);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run() {
+		changeText("Set...");
+		changeText("Go!");
+		changeText("");
+	}
+	
+	/**
+	*to implement later, after Character Team implements PlayerInterface
+	*This is used as a placeholder because early in the game design process, we use the method later, once we 
+	*learn how to create a Player
+	*/
+	private PlayerInterface getAPlayer() {
+	 return null;
+	}
+
+	/**
+	*to implement later, after EnemyTeam implements MoleInterface
+	*/
+	private MoleInterface getAMole() {
+	return null;
+	}
+
 
 }
